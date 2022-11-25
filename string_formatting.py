f29e9e13a1f9b6da6bf54bb791f9b4f4baa24ed9
Dilara Kızılkaya <74542327+vyperid@users.noreply.github.com>
2022-11-25 12:11:35 +0300

    Update string_formatting.py

--- string_formatting.py
+++ string_formatting.py
@@ -1,6 +1,6 @@
 def print_formatted(number):
+    width = len(bin(number)[2:])
     for i in range(1,number+1):
-        width = len(bin(number)[2:])
         print("{} {} {} {}".format((str(i).rjust(width)), (str(oct(i)[2:]).rjust(width)), (str(hex(i)[2:]).upper().rjust(width)), (str(bin(i)[2:]).rjust(width))))
         
 
